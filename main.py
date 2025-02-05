6149e0a39e2ae060bd1b07d3a04c93a778fcdc4d
numericunderflow06 <cs06theone@outlook.com>
2025-02-05 14:51:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from bagelcode.ml_model.model import (
+from utils import (
     CategoricalDenseModel,
     WeightedSum,
     AutoEncoder,
